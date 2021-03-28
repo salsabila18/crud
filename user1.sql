d8f725819a248f134e3613d9bd96dde6524d0ad2
salsabila18 <77769824+salsabila18@users.noreply.github.com>
2021-03-28 14:34:22 +0700

    Add files via upload

---
+++ user1.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 10 Feb 2021 pada 12.36
+-- Versi server: 10.3.16-MariaDB
+-- Versi PHP: 7.3.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `program_sederhana`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user1`
+--
+
+CREATE TABLE `user1` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `alamat` text NOT NULL,
+  `pekerjaan` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `user1`
+--
+
+INSERT INTO `user1` (`id`, `nama`, `alamat`, `pekerjaan`) VALUES
+(1, 'Andika Pramudya Nur Muntahar blalalaaaaaaaaaaaaaaa', 'Bandung', 'Programer'),
+(2, 'Santoso', 'Jakarta', 'Web Designer'),
+(3, 'Samsul', 'Sumedang', 'Pengusaha'),
+(6, 'Andi Pram', 'Bandung', 'Pengusaha');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `user1`
+--
+ALTER TABLE `user1`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `user1`
+--
+ALTER TABLE `user1`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
